6d631d13b96b4dc37cd8a6f5f557b212c403cd93
Thiago Cândido Rocha <thiagorocha192000@gmail.com>
2021-04-15 12:44:43 -0300

    Create main.c

---
+++ 3-FNC-SAT/main.c
@@ -0,0 +1,16 @@
+#include "Menu.h"
+void tabela_verdade(int colunas);
+
+int main(){
+    int op;
+    printf("Escolha o modo de operacao:\n\n   QUALQUER INTEIRO -> Automatico\n   2-> Manual\n");
+    scanf("%d",&op);
+    if(op==2){
+        Manual();
+    }
+    else{
+        Automatico();
+    }
+
+    return 0;
+}
